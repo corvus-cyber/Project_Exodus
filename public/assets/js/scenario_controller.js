78cdbe50f2d36d3134a26fc09973bb4b29eeb7c9
sgreilick <samgreilick@gmail.com>
2020-08-28 23:30:36 -0600

    preping the framework for the scenario renderer in the rpg page

---
+++ public/assets/js/scenario_controller.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+let scenarioDesc = document.getElementById("scDesc")
+let scenarioOpti = document.getElementById("scOpt")
+console.log(scenarioDesc,scenarioOpti)
+
+
+function renderScenario() {
+    //clears any currently shown answer options before rendering new ones
+    //$(scenarioOpti.children[0]).remove();
+
+    // console.log(questionDescription.textContent = questions[whichQuestion].description)
+
+    // //For loop that iterates through questions array and creates a new button for each one
+    // for (let i = 0; i < questions[whichQuestion].answers.length; i++) {
+    //     let answerOptions = document.createElement("a");
+    //     answerOptions.className = "btn btn-primary answerBtn";
+    //     answerOptions.id = "answer " + i;
+    //     answerOptions.textContent = questions[whichQuestion].answers[i];
+    //     questionLocation.appendChild(answerOptions);
+    // }
+};
+//renderScenario()
+
+
+
+
+});

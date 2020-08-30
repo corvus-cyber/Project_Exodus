283015646209c25c8e36e0400119a4a60e85be2a
sgreilick <samgreilick@gmail.com>
2020-08-30 15:24:53 -0600

    text field fills with current scenario body, type efect is bugged

--- public/assets/scenario_controller/scenario_controller.js
+++ public/assets/scenario_controller/scenario_controller.js
@@ -23,13 +23,8 @@ scenarioOpti.addEventListener("click", renderScenario)
 function renderScenario() {
     //clears any currently shown answer and text options before rendering new ones
     $(scenarioOpti.children).remove();
-
+    
     //render options
-    for (let i = 0; i < scenarios[scenarioVal].options.length; i++) {
-        let opt = `<button data=${i} type="button" class="btn btn-dark button-styling">${scenarios[scenarioVal].options[i].title}</button>`
-        scenarioOpti.insertAdjacentHTML("beforeend", opt);
-    }
-
     for (let i = 0; i < scenarios[scenarioVal].options.length; i++) {
         let opt = `<button data=${i} type="button" class="btn btn-dark button-styling">${scenarios[scenarioVal].options[i].title}</button>`
         scenarioOpti.insertAdjacentHTML("beforeend", opt);
@@ -37,27 +32,39 @@ function renderScenario() {
 
     scenarioOpti.addEventListener("click", function(event){
 
+        $(scenarioDesc.children).remove();
+
+        let selectedOpt = parseInt(event.target.getAttribute('data'));
+        optionVal = selectedOpt
+        console.log(optionVal);
         
-        let selectedOpt = event.target.getAttribute('data');
-        console.log(selectedOpt);
 
-
-        var content = event.target.textContent;
-
-        console.log(content)
+        console.log(scenarios[scenarioVal].text[1])
     
-        var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+        
     
         //console.log(ele)
-        $(ele).hide().appendTo(".description").each(function (i) {
-            $(this).delay(50 * i).css({
-                display: 'inline',
-                opacity: 0,
-            }).animate({
-                opacity: 1,
-                color: "white",
-            }, 100);
-        });
+   
+
+
+        for (let i = 0; i < scenarios[scenarioVal].text.length; i++) {
+            var content = scenarios[scenarioVal].text[i];
+
+            var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+            $(ele).hide().appendTo(".description").each(function (i) {
+                $(this).delay(50 * i).css({
+                    display: 'inline',
+                    opacity: 0,
+                }).animate({
+                    opacity: 1,
+                    color: "white",
+                }, 100);
+            });
+
+
+        }
+        
     });
 
 

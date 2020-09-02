3761554e3097018dac8dedf18e011eb97c42ca26
sgreilick <samgreilick@gmail.com>
2020-09-01 19:22:08 -0600

    navication within a single scenario working, self destruct handeler in place

--- public/assets/scenario_controller/scenario_controller.js
+++ public/assets/scenario_controller/scenario_controller.js
@@ -12,44 +12,38 @@ let scenarioDesc = document.getElementById("scDesc")
 let scenarioOpti = document.getElementById("scOpt")
 
 let scenarioVal = 0;
-let optionVal = 0;
+let optionVal;
+let currentScen;
+let selectedOpt;
 
 
 //-------------------delivery  system-----------------
-scenarioOpti.addEventListener("click", renderScenario)
-function renderScenario() {
-    
-    console.log(scenarios[scenarioVal].options); 
-    //clears any currently shown answer and text options before rendering new ones
-    $(scenarioOpti.children).remove();
-    
-    //render options
-    for (let i = 0; i < scenarios[scenarioVal].options.length; i++) {
-        //renders the title of the option and sets data value to that options v
-        let opt = `<button data=${i} type="button" class="btn btn-dark button-styling">${scenarios[scenarioVal].options[i].title} </button>`
-        scenarioOpti.insertAdjacentHTML("beforeend", opt);
-    }
+scenarioOpti.addEventListener("click", renderScenarioOpt);
+function renderScenarioOpt() {
 
-    //listen to options
-    scenarioOpti.addEventListener("click", function(event){
-        //remove options
-        $(scenarioDesc.children).remove();
-        //get the value of which option was selected
-        let selectedOpt = parseInt(event.target.getAttribute('data'));
+
+    //if same, rerender options
+    if (currentScen === scenarioVal) {
+        console.log("Same");
+        //clears any currently shown answer and text options before rendering new ones
+        $(scenarioOpti.children).remove();
+        console.log("Buttons Removed");
+        selectedOpt = parseInt(event.target.getAttribute('data'));
         optionVal = selectedOpt
-        //console.log(scenarios[scenarioVal].options[1].text[0]);
 
-        //prepares the content variable
-        let content;
-        //without this the content will start with "undefined"
-        content = "";
-    
+        //render options
         scenarioVal = scenarios[scenarioVal].options[optionVal].toScenario
-        //console.log("scenval"+scenarioVal)
+
 
         //render option text
-        for (let i = 0; i < scenarios[scenarioVal].options[optionVal].text.length; i++) {  
-                content += scenarios[scenarioVal].options[optionVal].text[i]+" ";
+        let content;
+
+        //without this the content will start with "undefined"
+        content = "";
+        //console.log("scenval"+scenarioVal)
+        $(scenarioDesc.children).remove();
+        for (let i = 0; i < scenarios[scenarioVal].options[optionVal].text.length; i++) {
+            content += scenarios[scenarioVal].options[optionVal].text[i] + " ";
         }
 
         var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
@@ -65,27 +59,93 @@ function renderScenario() {
         });
 
 
-        
-        //delete scenarios[scenarioVal].options[optionVal]
-        if (scenarios[scenarioVal].options[optionVal].actions.includes("selfDestruct")) {
+
+
+
+        if (scenarios[currentScen].options[optionVal].actions.includes("selfDestruct")) {
             console.log("destruction!!!!!");
-            scenarios[scenarioVal].options.splice(optionVal)
+            scenarios[currentScen].options.splice(optionVal, 1)
         }
         console.log(scenarios[scenarioVal].options);
 
-        return renderScenario()
-        
-    });
+        //button renderer
+        for (let i = 0; i < scenarios[scenarioVal].options.length; i++) {
+            //renders the title of the option and sets data value to that options v
+            let opt = `<button data=${i} type="button" class="btn btn-dark button-styling">${scenarios[scenarioVal].options[i].title} </button>`
+            scenarioOpti.insertAdjacentHTML("beforeend", opt);
+        }
+        console.log("Buttons Created");
 
 
-    // console.log(questionDescription.textContent = questions[whichQuestion].description)
 
-    // //For loop that iterates through questions array and creates a new button for each one
-    // for (let i = 0; i < questions[whichQuestion].answers.length; i++) {
-    //     let answerOptions = document.createElement("a");
-    //     answerOptions.className = "btn btn-primary answerBtn";
-    //     answerOptions.id = "answer " + i;
-    //     answerOptions.textContent = questions[whichQuestion].answers[i];
-    //     questionLocation.appendChild(answerOptions);
-    // }
+
+
+
+    }
+    //if  not same, render scenario
+    else {
+        console.log("Not Same");
+        currentScen = scenarioVal
+        //clears any currently shown answer and text options before rendering new ones
+        $(scenarioOpti.children).remove();
+        console.log("Buttons Removed");
+
+        //render options
+        for (let i = 0; i < scenarios[scenarioVal].options.length; i++) {
+            //renders the title of the option and sets data value to that options v
+            let opt = `<button data=${i} type="button" class="btn btn-dark button-styling">${scenarios[scenarioVal].options[i].title} </button>`
+            scenarioOpti.insertAdjacentHTML("beforeend", opt);
+        }
+        console.log("Buttons Created");
+
+        let content;
+        //without this the content will start with "undefined"
+        content = "";
+        //render text
+
+        $(scenarioDesc.children).remove();
+        for (let i = 0; i < scenarios[scenarioVal].text.length; i++) {
+            content += scenarios[scenarioVal].text[i] + " ";
+        }
+
+        var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+        $(ele).hide().appendTo(".description").each(function (i) {
+            $(this).delay(50 * i).css({
+                display: 'inline',
+                opacity: 0,
+            }).animate({
+                opacity: 1,
+                color: "white",
+            }, 100);
+        });
+
+    }
+
+
+
+
+
+
+
+
+    //prepares the content variable
+
+
+
+    // if current val is not = to scenval render scenario, else render option 
+
+    //listen to options
+    //scenarioOpti.addEventListener("click", function (event) {
+    //remove options
+    //$(scenarioDesc.children).remove();
+    //get the value of which option was selected
+
+
+
+
+    //return renderScenarioOpt()
+
+    //});
+
 };

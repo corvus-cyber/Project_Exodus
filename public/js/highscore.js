8f43c41e2c21cc8843d404592d88caff023435eb
sgreilick <samgreilick@gmail.com>
2020-09-02 17:05:44 -0600

    close to functional death counter, counter is dropped on highscore reload

--- public/js/highscore.js
+++ public/js/highscore.js
@@ -1,27 +1,4 @@
-let deathCounter = 0;
 
-function updateDeath(){
-    deathCounter += 1;
-}
-
-$("#submit-score").on("click", function(event){
-    event.preventDefault();
-
-    const newUser = {
-        username: $("#log-score").val().trim(),
-        score: deathCounter
-    };
-    
-    $.ajax("/highscore", {
-        type: "POST",
-        data: newUser
-    }).then(
-        function(){
-            console.log("new user created")
-            location.reload();
-        }
-    )
-})
 
 
 

bc665f42309e0fa3e4675277d3cc3263e4294af3
Ammon Jeffery <savingphace@gmail.com>
2020-09-01 20:02:04 -0600

    changed toScenario values in starting_scenario

--- public/assets/scenario_controller/scenarios/1_starting_scenario.js
+++ public/assets/scenario_controller/scenarios/1_starting_scenario.js
@@ -14,7 +14,7 @@ let scenario_1={
             title: "Look down and Examine Yourself",
             text: ["You look down to better gauge your physical state, and it becomes instantly clear why you ache so much. Your arms and legs are covered in stitches surrounding patches of skin that are clearly not your own. Some of them are light, others are dark. One patch on your right arm appears to be rotten and decaying."],
             actions:[],
-            toScenario: 1
+            toScenario: 0
         },
         {
             id: 2,
@@ -32,7 +32,7 @@ let scenario_1={
                     "Day 3: Prep work for operation complete. Subject ready for experiment. Graft 1-5 success. Subject shows promise for organ transfer.",
                     "This writing goes on for pages. It’s clear that you aren’t the first person trapped here. You still have no idea what this place is, but one thing is clear: you need to get the hell out."   ],
             actions:[],
-            toScenario: 1
+            toScenario: 0
         },
         {
             id: 4,

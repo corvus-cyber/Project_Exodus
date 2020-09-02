05011722fb55ba85eaffe72af7ddc54a61e8a305
Ammon Jeffery <savingphace@gmail.com>
2020-09-01 20:18:55 -0600

    made small correction in scenario2

--- public/assets/scenario_controller/scenarios/2_scenario_hallway2.js
+++ public/assets/scenario_controller/scenarios/2_scenario_hallway2.js
@@ -4,7 +4,7 @@ let scenario_2={
     text: [
         "You find yourself in a long corridor, illuminated by several dim light bulbs lining the ceiling. Dark grey tiles stretch out in either direction. The portions of walls that aren’t rotted or peeling are covered in grotesque graffiti. The ceiling above is plastered with yellowed water stains. Several trays line the hallways along the walls. Bloody footprints form interweaving scarlet paths along the floor, including into the room behind you.",
         "Before you, a darkened hallway stretches before you. You can barely make out a door on the opposite end of the hallway. A broken exit sign hangs from the top of the doorway.",
-        "A single flickering light faintly illuminates the room. The white paint on the walls is peeling, and the parts that aren’t are covered in graffiti. Pools of dark ichor cover the floor, faintly reflecting the light above. Several  syringes lie scattered across the floor, some of them partially filled with unknown liquids.",
+        "Down the hallway to your right stands a wooden door with a tarnished plaque that you can’t quite make out at this distance, although it looks like it might be an office of some sort. This hallway appears better lit than other parts of the facility you’ve seen.",
         "Down the hallway to your left is a door partly ajar that reads “Operation”. A cold, white light pours through the doorway You can hear whimpering coming from this direction...It almost sounds like...a dog?",
         "What do you do?"
     ],

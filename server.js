7371d6b90fc31557a02d96bcc1d2a42538316934
Dilan Li <dilanli0706@gmail.com>
2020-09-04 00:18:30 -0600

    highscore page route

--- server.js
+++ server.js
@@ -10,19 +10,16 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// var Highscore = require("./models/highscore.js");
 
 // Set Handlebars.
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Requiring our routes
-
 var routes = require("./routes/exodus-controller.js");
 app.use(routes);
 
-db.sequelize.sync();
+db.sequelize.sync({force:true});
 app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
 });

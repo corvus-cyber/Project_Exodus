20bcc1fe56327bc0828b7387645809015329e310
sgreilick <samgreilick@gmail.com>
2020-09-07 18:10:00 -0600

    type effect victory page"

--- public/js/victory.js
+++ public/js/victory.js
@@ -21,5 +21,31 @@ function runDoorFade(){
     
 };
 
+
+let winContent = [
+    
+]
+
+$(scenarioDesc.children).remove();
+        for (let i = 0; i < scenarios[scenarioVal].text.length; i++) {
+            content = scenarios[scenarioVal].text[i];
+            
+            let p = `<p id="scenPara_${i}"></p>`
+            scenarioDesc.insertAdjacentHTML("beforeend", p);
+            
+           
+            var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+            $(ele).hide().appendTo("#scenPara_"+i+"").each(function (i) {
+                $(this).delay(50 * i).css({
+                    display: 'inline',
+                    opacity: 0,
+                }).animate({
+                    opacity: 1,
+                    color: "white",
+                }, 100);
+            });
+        }
+
 runDoorFade();
 })
